f48fa013ebeebe27652ab46f2232ad6652cdfd58
Ahmad Nidal Delea <a7mad.delea@gmail.com>
2024-10-30 18:41:59 +0300

    Finish app

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 ToDo flutter app with Provider state managment.
 
 # App05
+<div style="display: flex; flex-wrap: wrap; align-items: center;">
+    <img src="./images/screen.jpg" alt="Home" style="margin-right: 10px; width: 300px; height: auto;">
+</div>
--- lib/providers/task_data_provider.dart
+++ lib/providers/task_data_provider.dart
@@ -3,13 +3,7 @@ import 'package:flutter/material.dart';
 import '../models/task.dart';
 
 class TaskDataProvider extends ChangeNotifier {
-  List<Task> tasks = [
-    Task(task: 'task1'),
-    Task(task: 'task2'),
-    Task(task: 'task3'),
-    Task(task: 'task4'),
-    Task(task: 'task5'),
-  ];
+  List<Task> tasks = [];
 
   int get count => tasks.length;
 
